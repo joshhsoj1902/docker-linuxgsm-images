9f4063ca4e57f5913646247d0fff992eee7fcf93
Josh Bryans <josh@joshman.ca>
2024-11-24 10:06:13 -0500

    more debug

--- images/sdtd-base/.tests/scripts/docker/00-render_serverconfig.sh
+++ images/sdtd-base/.tests/scripts/docker/00-render_serverconfig.sh
@@ -4,6 +4,11 @@ export LGSM_SERVER_NAME="My Game Host"
 
 gomplate -f /layers/001-base/serverfiles/sdtdserver.xml.gomplate -o /tmp/gomplate/gomplate-serverconfig.xml
 
-diff -w --color=always --unified=3 /data/serverfiles/serverconfig.xml /tmp/gomplate//gomplate-serverconfig.xml
+echo "serverfiles"
+ls -ltr /data/serverfiles
 
-echo "hello world"
+echo ""
+echo "gomplated"
+ls -ltr /tmp/gomplate
+
+diff -w --color=always --unified=3 /data/serverfiles/serverconfig.xml /tmp/gomplate//gomplate-serverconfig.xml
