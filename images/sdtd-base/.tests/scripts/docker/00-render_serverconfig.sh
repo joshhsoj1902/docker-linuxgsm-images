e4769f94dce9839928df9325fa4779ffb6dd8f17
Josh Bryans <josh@joshman.ca>
2024-11-24 12:41:53 -0500

    Change everything (move test to do file)

--- images/sdtd-base/.tests/scripts/docker/00-render_serverconfig.sh
+++ images/sdtd-base/.tests/scripts/docker/00-render_serverconfig.sh
@@ -4,11 +4,4 @@ export LGSM_SERVER_NAME="My Game Host"
 
 gomplate -f /layers/001-base/serverfiles/sdtdserver.xml.gomplate -o /tmp/gomplate/gomplate-serverconfig.xml
 
-echo "serverfiles"
-ls -ltr /data/serverfiles
-
-echo ""
-echo "gomplated"
-ls -ltr /tmp/gomplate
-
 diff -w --color=always --unified=3 /data/serverfiles/serverconfig.xml /tmp/gomplate//gomplate-serverconfig.xml
