af4fc9040bd059c4aac8e6112679d1954fe7f198
Josh Bryans <josh@joshman.ca>
2024-11-24 09:52:23 -0500

    fixing ci failures

--- images/sdtd-base/.tests/scripts/docker/00-render_serverconfig.sh
+++ images/sdtd-base/.tests/scripts/docker/00-render_serverconfig.sh
@@ -5,3 +5,5 @@ export LGSM_SERVER_NAME="My Game Host"
 gomplate -f /layers/001-base/serverfiles/sdtdserver.xml.gomplate -o /tmp/gomplate/gomplate-serverconfig.xml
 
 diff -w --color=always --unified=3 /data/serverfiles/serverconfig.xml /tmp/gomplate//gomplate-serverconfig.xml
+
+echo "hello world"
