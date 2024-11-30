3f4a7a455ff06448fdea99143bb6c9b6e9085879
Josh Bryans <josh@joshman.ca>
2024-11-30 12:06:45 -0500

    Add sleep to hopefully address palworld test flake

--- images/palworld-base/.tests/scripts/local/00-render_PalWorldSettings.sh
+++ images/palworld-base/.tests/scripts/local/00-render_PalWorldSettings.sh
@@ -1,5 +1,8 @@
 echo "Validating gomplate produces default config"
 
+echo "sleeping for 60 seconds to allow testing server to properly start"
+sleep 60
+
 ./do dockerCopyFrom testing /data/serverfiles/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini /tmp/vanilla-palworld.ini
 ./do dockerCopyFrom linuxgsm /data/serverfiles/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini /tmp/gomplate-palworld.cfg
 
