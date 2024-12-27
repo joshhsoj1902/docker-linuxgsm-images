18d60ed3beb3161363f37dc5b1ac120f0091c240
Josh Bryans <josh@joshman.ca>
2024-12-27 18:32:32 -0500

    [palworld] Set default save file GUID

--- images/palworld-base/.tests/scripts/local/00-render_PalWorldSettings.sh
+++ images/palworld-base/.tests/scripts/local/00-render_PalWorldSettings.sh
@@ -1,9 +1,18 @@
+SUDO=''
+if [[ $EUID != 0 ]] && [[ "$USER" != "gha" ]]; then
+    SUDO='sudo'
+fi
+
 echo "Validating gomplate produces default config"
 
 echo "sleeping for 60 seconds to allow testing server to properly start"
 sleep 60
 
-./do dockerCopyFrom testing /data/serverfiles/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini /tmp/vanilla-palworld.ini
-./do dockerCopyFrom linuxgsm /data/serverfiles/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini /tmp/gomplate-palworld.cfg
+./do dockerCopyFrom testing /data/serverfiles/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini /tmp/vanilla-palworld-worldsettings.ini
+./do dockerCopyFrom linuxgsm /data/serverfiles/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini /tmp/gomplate-palworld-worldsettings.cfg
+diff -w --color=always --unified=3 /tmp/vanilla-palworld-worldsettings.ini /tmp/gomplate-palworld-worldsettings.cfg
 
-diff -w --color=always --unified=3 /tmp/vanilla-palworld.ini /tmp/gomplate-palworld.cfg
+./do dockerCopyFrom testing /data/serverfiles/Pal/Saved/Config/LinuxServer/GameUserSettings.ini /tmp/vanilla-palworld-usersettings.ini
+./do dockerCopyFrom linuxgsm /data/serverfiles/Pal/Saved/Config/LinuxServer/GameUserSettings.ini /tmp/gomplate-palworld-usersettings.cfg
+$SUDO sed -i "s/DedicatedServerName=.*/DedicatedServerName=22AD4EA497F14EE4800AF1108166863B/g" /tmp/vanilla-palworld-usersettings.ini
+diff -w --color=always --unified=3 /tmp/vanilla-palworld-usersettings.ini /tmp/gomplate-palworld-usersettings.cfg
